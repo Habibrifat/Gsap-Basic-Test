c7ede82501d16ff21a32a570de6eee6498a9df6d
Habibrifat <shah.newaz.habib.rifat@gmail.com>
2024-11-02 23:26:50 +0600

    use vite

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+// vite.config.js
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "src", // Set root directory if your index.html is in 'src'
+  publicDir: "../public", // Directory for static assets
+});
